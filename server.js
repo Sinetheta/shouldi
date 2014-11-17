b3b13fe75e5b2dfc35fa2b2bb8a7979241f43020
Kevin Attfield <kevin@freerunningtech.com>
2014-11-16 17:38:32 -0800

    Add placeholder endpoint
    Change-Id: Id4fd805a1c25d9c91bcdc6477a6c1acd8cc24659

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var router = express.Router();
+
+router.get('/shouldi', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use(router)
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Magic happens on port ' + port);
