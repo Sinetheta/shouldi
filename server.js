b7f588e0c6f20177388363f892347958824d1779
Kevin Attfield <kevin@freerunningtech.com>
2014-11-16 21:21:10 -0800

    Add CORS headers
    Change-Id: Ia6f376f7b940947e3e40c1f26b91a56cd3f3ad36

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ var chooseRandom = function(array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var router = express.Router();
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
 
+//Our single endpoint
+var router = express.Router();
 var answers = require('./answers.json');
 router.get('/shouldi', function(req, res) {
   var answer = chooseRandom(answers);
   res.json(answer);
 });
-
 app.use(router)
 
+//404 any other requests
 app.use(function(req, res){
   res.status(404).json({ message: 'Stai cercando qualcuno? ' });
 });
 
+//Fire up server
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Magic happens on port ' + port);
